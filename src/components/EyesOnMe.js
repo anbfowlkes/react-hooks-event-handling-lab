16a7fecf747352389a69af91cc9363cadc3a0ff8
Aaron Fowlkes <afowlkes21@gmail.com>
2022-07-21 11:58:13 -0400

    commit tree 63c2cf4761fd75778800016088d13f2f1de6df2e

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+
+
+
+const EyesOnMe = () => {
+    const handleFocus = (e) => {
+        console.log('Good!')
+    }
+    const handleBlur = (e) => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    )
+}
+
+
+
+export default EyesOnMe
